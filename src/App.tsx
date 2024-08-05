53cf5e71dbe0df7b21a4403a20a01289f58f1689
Alan Torres <alan.trrs.ngls@gmail.com>
2024-08-04 20:45:51 -0700

    Setup layout links

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,15 @@
+import { 
+  HomeIcon, 
+  Cog6ToothIcon, 
+  CubeIcon, 
+  BuildingStorefrontIcon, 
+  WrenchScrewdriverIcon, 
+  TruckIcon,
+  ChartBarIcon
+} from '@heroicons/react/24/outline'
 import { Button } from './lib/button'
 import { SidebarLayout } from './lib/sidebar-layout'
-import { Sidebar, SidebarHeader, SidebarBody, SidebarFooter, SidebarItem } from './lib/sidebar'
+import { Sidebar, SidebarHeader, SidebarBody, SidebarFooter, SidebarItem, SidebarSection, SidebarLabel } from './lib/sidebar'
 import { Navbar, NavbarItem } from './lib/navbar'
 import { BrowserRouter } from 'react-router-dom'
 
@@ -15,22 +24,46 @@ const AppLayout = () => {
       sidebar={
         <Sidebar>
           <SidebarHeader>
-            <SidebarItem href='/'>Dashboard</SidebarItem>
+            <SidebarItem href='/'>
+              <HomeIcon />
+              <SidebarLabel>Dashboard</SidebarLabel>
+            </SidebarItem>
           </SidebarHeader>
           <SidebarBody>
-            <SidebarItem href='/parts'>Parts</SidebarItem>
-            <SidebarItem href='/inventory'>Inventory</SidebarItem>
-            <SidebarItem href='/production'>Production</SidebarItem>
-            <SidebarItem href='/procurement'>Procurement</SidebarItem>
-        </SidebarBody>
+            <SidebarSection>
+              <SidebarItem href='/mrp'>
+                <Cog6ToothIcon />
+                <SidebarLabel>MRP</SidebarLabel>
+              </SidebarItem>
+              <SidebarItem href='/parts'>
+                <CubeIcon />
+                <SidebarLabel>Parts</SidebarLabel>
+              </SidebarItem>
+              <SidebarItem href='/inventory'>
+                <BuildingStorefrontIcon />
+                <SidebarLabel>Inventory</SidebarLabel>
+              </SidebarItem>
+              <SidebarItem href='/production'>
+                <WrenchScrewdriverIcon />
+                <SidebarLabel>Production</SidebarLabel>
+              </SidebarItem>
+              <SidebarItem href='/procurement'>
+                <TruckIcon />
+                <SidebarLabel>Procurement</SidebarLabel>
+              </SidebarItem>
+            </SidebarSection>
+          </SidebarBody>
           <SidebarFooter>
-            <SidebarItem href='/settings'>Settings</SidebarItem>
+            <SidebarItem href='/settings'>
+              <ChartBarIcon />
+              <SidebarLabel>Analytics</SidebarLabel>
+            </SidebarItem>
           </SidebarFooter>
         </Sidebar>
       }
     >
       <h1 className='text-2xl font-bold mb-4'>Optimal MRP</h1>
-      <Button>Get Started</Button>
+      <Button outline>Get Started</Button>
     </SidebarLayout>
   )
 }
---
+++ src/components/mrp/mrp.page.tsx
@@ -0,0 +1,10 @@
+import Page from "../../lib/page"
+
+const MRP = () => {
+  return (
+    <Page>
+      <h1>MRP</h1>
+    </Page>
+  )
+}
+export default MRP
