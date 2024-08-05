17d243e315b4d3b054c12e1b929b9fb90c639056
Alan Torres <alan.trrs.ngls@gmail.com>
2024-08-04 18:09:32 -0700

    Setting up layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,45 @@
-
-import './App.css'
 import { Button } from './lib/button'
-import Page from './lib/page'
+import { SidebarLayout } from './lib/sidebar-layout'
+import { Sidebar, SidebarHeader, SidebarBody, SidebarFooter, SidebarItem } from './lib/sidebar'
+import { Navbar, NavbarItem } from './lib/navbar'
+import { BrowserRouter } from 'react-router-dom'
 
-function App() {
- 
+const AppLayout = () => {
   return (
-    <Page>
-      <h1>Optimal MRP</h1>
-      <Button> Hey</Button>
-    </Page>
+    <SidebarLayout
+      navbar={
+        <Navbar>
+          <NavbarItem>Optimal MRP</NavbarItem>
+        </Navbar>
+      }
+      sidebar={
+        <Sidebar>
+          <SidebarHeader>
+            <SidebarItem href='/'>Dashboard</SidebarItem>
+          </SidebarHeader>
+          <SidebarBody>
+            <SidebarItem href='/parts'>Parts</SidebarItem>
+            <SidebarItem href='/inventory'>Inventory</SidebarItem>
+            <SidebarItem href='/production'>Production</SidebarItem>
+            <SidebarItem href='/procurement'>Procurement</SidebarItem>
+        </SidebarBody>
+          <SidebarFooter>
+            <SidebarItem href='/settings'>Settings</SidebarItem>
+          </SidebarFooter>
+        </Sidebar>
+      }
+    >
+      <h1 className='text-2xl font-bold mb-4'>Optimal MRP</h1>
+      <Button>Get Started</Button>
+    </SidebarLayout>
+  )
+}
+
+const App = () => {
+  return (
+    <BrowserRouter>
+      <AppLayout />
+    </BrowserRouter>
   )
 }
 
